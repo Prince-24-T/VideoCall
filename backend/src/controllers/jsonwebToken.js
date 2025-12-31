b026eae93559d46d1ae709c3c4cc367e94973ef6
Prince-24-T <pt023150@gmail.com>
2025-12-31 20:39:53 +0530

    again not working

--- backend/src/controllers/jsonwebToken.js
+++ backend/src/controllers/jsonwebToken.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 const jsoWebToken = (id) => {
   const token = jwt.sign({ id }, process.env.secretKey, {
-    expiresIn: Math.floor(Date.now() / 1000) + 60 * 60,
+    expiresIn: "1h",
   });
   return token;
 };
